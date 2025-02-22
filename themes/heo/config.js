5d94633c913d3f32998d336a33ae210daf75ccae
andyandybeginner <65121670+andyandybeginner@users.noreply.github.com>
2025-02-22 16:33:07 +0800

    english version theme heo

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ HEO_SITE_CREATE_TIME: '2024-12-01', // 建站日期，用于计算网站运行�
 
 // 首页顶部通知条滚动内容，如不需要可以留空 []
 HEO_NOTICE_BAR: [
-{ title: '亲爱的客户，一站式管理所有保单', url: '' },
-{ title: '亲！常回来看看！', url: '' },
+{ title: 'Dear Clients, Welcome Back!', url: '' },
+{ title: 'Check! New promotions are available', url: '' },
 ],
 
 // 英雄区左右侧组件颠倒位置
@@ -18,30 +18,30 @@ HEO_HERO_REVERSE: false,
 HEO_HERO_BODY_REVERSE: false,
 
 // 英雄区(首页顶部大卡)
-HEO_HERO_TITLE_1: '即时快速',
-HEO_HERO_TITLE_2: '找到所想',
-HEO_HERO_TITLE_3: '一站式解决方案',
-HEO_HERO_TITLE_4: '限时特惠',
-HEO_HERO_TITLE_5: '最新优惠信息',
+HEO_HERO_TITLE_1: 'Fast & Instant!',
+HEO_HERO_TITLE_2: 'Find What You Want!',
+HEO_HERO_TITLE_3: 'Your One-Stop Solution!',
+HEO_HERO_TITLE_4: 'Hot Deals & Offers!',
+HEO_HERO_TITLE_5: 'Limited-Time Deals!',
 // 英雄区遮罩文字
-HEO_HERO_COVER_TITLE: '随便逛逛',
+HEO_HERO_COVER_TITLE: 'Just Browsing?',
 
 // 英雄区显示三个置顶分类
-HEO_HERO_CATEGORY_1: { title: '最新优惠', url: '/tag/最新优惠' },
-HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/tag/实用教程' },
-HEO_HERO_CATEGORY_3: { title: '新加坡资讯', url: '/tag/实用资讯' },
+HEO_HERO_CATEGORY_1: { title: 'Latest Deals', url: '/tag/Latest Deals' },
+HEO_HERO_CATEGORY_2: { title: 'Handy Guides', url: '/tag/Handy Guides' },
+HEO_HERO_CATEGORY_3: { title: 'Singapore Buzz', url: '/tag/Singapore Buzz' },
 
 // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-HEO_HERO_RECOMMEND_POST_TAG: '促销',
+HEO_HERO_RECOMMEND_POST_TAG: 'Promotion',
 HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
 //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
 // 右侧个人资料卡牌欢迎语，点击可自动切换
 HEO_INFOCARD_GREETINGS: [
-'你好！我是',
-'🔍 分享与热心帮助',
-'🏃 脚踏实地行动派',
-'🧱 新加坡专业理财教练'
+'Hello! I am',
+'💡 Sharing & always ready to help',
+'🚀 A go-getter who gets things done',
+'🏆 A professional financial coach in Singapore'
 ],
 
 // 个人资料底部按钮
@@ -50,7 +50,7 @@ HEO_INFO_CARD_ICON1: 'fas fa-user',
 HEO_INFO_CARD_URL2: 'https://www.linkedin.com/in/guhaoranandy/',
 HEO_INFO_CARD_ICON2: 'fa-brands fa-linkedin',
 HEO_INFO_CARD_URL3: '[]()',
-HEO_INFO_CARD_TEXT3: '了解更多',
+HEO_INFO_CARD_TEXT3: 'Learn More',
 
 // 用户技能图标
 HEO_GROUP_ICONS: [
@@ -81,9 +81,9 @@ color_2: '#ffa31a'
 ],
 
 HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-HEO_SOCIAL_CARD_TITLE_3: '点击发送WhatsApp',
+HEO_SOCIAL_CARD_TITLE_1: 'Community Channel',
+HEO_SOCIAL_CARD_TITLE_2: 'Join Our Community for Discussions & Sharing',
+HEO_SOCIAL_CARD_TITLE_3: 'Click to Send WhatsApp'
 HEO_SOCIAL_CARD_URL: 'https://wa.me/message/KE5RR5YOIEHOG1',
 
 // *****  以下配置无效，只是预留开发 ****
